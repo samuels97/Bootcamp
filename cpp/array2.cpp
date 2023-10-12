b399afe5d699d86cfc08e720dd6c9f7b5fc4a86c
kalumba Samuel <kalumbasml@gmail.com>
2023-10-12 15:52:27 +0300

    pointer dereferencing and casting

--- cpp/array2.cpp
+++ cpp/array2.cpp
@@ -5,7 +5,10 @@ using namespace std;
 int main ()
 
 {
-
+int c = 5;
+int* pp = &c;
+void* pv = pp;
+cout << "Pointer dereferencing & typecasting change: " << *(static_cast<int*>(pv)) << endl << endl;
  // an array with 5 rows and 2 columns.
 
  int a[5][2] = { {0,0}, {1,2}, {2,4}, {3,6},{4,8}};
