16037a74cc1489ccfce7c6d6587939c62440cfed
Samuels7 <66149372+samuels97@users.noreply.github.com>
2024-02-20 15:44:41 +0300

    Update string_tester.cpp

--- cpp/string_tester.cpp
+++ cpp/string_tester.cpp
@@ -9,4 +9,12 @@ int main(){
 
   string phrase = word1 + " " + word2 + word3;
   cout << "The phrase is: " << phrase << "\n\n";
+
+  cout << "The phrase has: " << phrase.size() << " characters in it\n\n";
+  cout << "The character at position 0 is: " << phrase[0] << "\n\n";
+  cout << "Changing the character at position 0 \n\n";
+  phrase[0] = "L";
+
+
+  return 0;
 }
